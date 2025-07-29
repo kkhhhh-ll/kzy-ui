a2e896ccabcbfec53753a5fb1526df0e69b3c2b5
kk <294378050@qq.com>
2025-07-29 15:54:24 +0800

    feat: :sparkles: 模块介绍

---
+++ docs/blogs/javascript/module.md
@@ -0,0 +1,110 @@
+## 浏览器加载
+
+浏览器通过script标签加载js代码
+
+```
+<!-- 页面内嵌的脚本 -->
+<script type="application/javascript">
+  // module code
+</script>
+<!-- 外部脚本 -->
+<script type="application/javascript" src="path/to/myModule.js">
+</script>
+```
+
+默认情况下，浏览器是同步加载 JavaScript 脚本，即渲染引擎遇到<script>标签就会停下来，等到执行完脚本，再继续向下渲染。如果是外部脚本，还必须加入脚本下载的时间。
+
+## 浏览器加载ES6模块
+
+浏览器加载 ES6 模块，也使用<script>标签，但是要加入type="module"属性。
+
+```
+<script type="module" src="./foo.js"></script>
+```
+
+浏览器对于带有type="module"的<script>，都是异步加载，不会造成堵塞浏览器，即等到整个页面渲染完，再执行模块脚本，等同于打开了<script>标签的defer属性。
+
+## ES6与CommomJs模块差异
+1、cjs输出的值的拷贝，esm输出的是值的引用；<br>
+2、cjs运行时加载，esm是编译时输出接口。<br>
+CommonJS 模块输出的是值的拷贝，一旦输出一个值，模块内部的变化就影响不到这个值。<br>
+JS 引擎对脚本静态分析的时候，遇到模块加载命令import，就会生成一个只读引用。等到脚本真正执行时，再根据这个只读引用，到被加载的那个模块里面去取值。
+
+## Node.js加载
+Node.js 要求 ES6 模块采用.mjs后缀文件名。也就是说，只要脚本文件里面使用import或者export命令，那么就必须采用.mjs后缀名。Node.js 遇到.mjs文件，就认为它是 ES6 模块，默认启用严格模式，不必在每个模块文件顶部指定"use strict"。<br>
+.mjs文件总是以 ES6 模块加载，.cjs文件总是以 CommonJS 模块加载，.js文件的加载取决于package.json里面type字段的设置。<br>
+
+### main
+package.json文件有两个字段可以指定模块的入口文件：main和exports。比较简单的模块，可以只使用main字段，指定模块加载的入口文件。
+```
+// ./node_modules/es-module-package/package.json
+{
+  "type": "module",
+  "main": "./src/index.js"
+}
+```
+### exports
+优先级高于main字段。
+#### 子目录别名
+package.json文件的exports字段可以指定脚本或子目录的别名。
+```
+// ./node_modules/es-module-package/package.json
+{
+  "exports": {
+    "./submodule": "./src/submodule.js"
+  }
+}
+// 代码指定src/submodule.js别名为submodule，然后就可以从别名加载这个文件。
+```
+#### main 的别名
+exports字段的别名如果是.，就代表模块的主入口，优先级高于main字段，并且可以直接简写成exports字段的值。
+```
+{
+  "exports": {
+    ".": "./main.js"
+  }
+}
+// 等同于
+{
+  "exports": "./main.js"
+}
+```
+#### 条件加载
+利用.这个别名，可以为 ES6 模块和 CommonJS 指定不同的入口。目前，这个功能需要在 Node.js 运行的时候，打开--experimental-conditional-exports标志。
+```
+{
+  "type": "module",
+  "exports": {
+    ".": {
+      "require": "./main.cjs",
+      "default": "./main.js"
+    }
+  }
+}
+
+// 简写
+{
+  "exports": {
+    "require": "./main.cjs",
+    "default": "./main.js"
+  }
+}
+```
+### ES6加载CommonJs
+ES6 模块通过Node内置的module.createRequire()方法加载 CommonJS 模块
+```
+// cjs.cjs
+module.exports = 'cjs';
+// esm.mjs
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const cjs = require('./cjs.cjs');
+cjs === 'cjs'; // true
+```
+### CommonJS 模块加载 ES6 模块
+CommonJS 的require命令不能加载 ES6 模块，会报错，只能使用import()这个方法加载。
+```
+(async () => {
+  await import('./my-app.mjs');
+})();
+```
--- index.js
+++ index.js
@@ -1,2 +1,193 @@
-import { parse } from "path";
-console.log(parse("C:\\Users\\kyk\\Desktop\\gb\\kzy-ui\\src\\index.js"));
+// import { parse } from "path";
+// console.log(parse("C:\\Users\\kyk\\Desktop\\gb\\kzy-ui\\src\\index.js"));
+
+// map，两数之和
+function getArr(arr, target) {
+    const map = new Map()
+    arr.forEach((item, index) => {
+        if (map.has(target - item)) {
+            console.log([index, map.get(target - item)])
+        } else {
+            map.set(item, index)
+        }
+    }
+    )
+}
+getArr([2, 7, 10, 11], 9)
+
+// 字母异构
+function getStr(strArr) {
+    const map = new Map()
+    strArr.forEach(item => {
+        const sortStr = Array.from(item).sort().toString()
+        if (!map.has(sortStr)) {
+            map.set(sortStr, [])
+        }
+        map.get(sortStr).push(item)
+
+    })
+    console.log(map)
+}
+getStr(['tea', 'eat', 'aet', 'tan'])
+
+// 最长连续序列
+function getMax(arr) {
+    const set = new Set(arr)
+    let maxLength = 1
+    for (let i = 0; i < arr.length; i++) {
+        // 序列的起点
+        let num = arr[i]
+        let currentLength = 0
+        if (!set.has(num - 1)) {
+            while (set.has(num++)) {
+                currentLength++
+            }
+            maxLength = Math.max(maxLength, currentLength)
+        }
+    }
+    console.log(maxLength)
+}
+getMax([1, 2, 3, 4, 5, 100, 101, 102, 103, 104, 105, 106, 200])
+
+// 移动0，指针->左右指针的时候，需要人为的改变left，right的值
+function moveZero(arr) {
+    let left = 0
+    for (let right = 0; right < arr.length; right++) {
+        if (arr[right] !== 0) {
+            if (left !== right) {
+                let temp = arr[left]
+                arr[left] = arr[right]
+                arr[right] = temp
+            }
+            left++
+        }
+    }
+    console.log(arr)
+}
+moveZero([1, 2, 0, 0, 1, 5, 10])
+
+
+// 盛水问题
+function getMaxArea(arr) {
+    let left = 0;
+    let right = arr.length - 1
+    let maxArea = 0
+    while (left < right) {
+        const width = right - left
+        const minHeight = Math.min(arr[left], arr[right])
+        const currentArea = width * minHeight
+        maxArea = Math.max(maxArea, currentArea)
+        if (arr[left] < arr[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+    console.log(maxArea)
+}
+getMaxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+
+// 三数之和
+function getThreeAdd(arr) {
+    // 排序后的数组
+    const sortArr = arr.sort()
+    let res = []
+    for (let i = 0; i < sortArr.length; i++) {
+        if (sortArr[i] > 0) break
+        // 跳过重复的第一个数
+        if (i > 0 && sortArr[i] == sortArr[i - 1]) continue;
+        let left = i + 1
+        let right = sortArr.length - 1
+        while (left < right) {
+            let sum = sortArr[i] + sortArr[left] + sortArr[right]
+            if (sum === 0) {
+                res.push([sortArr[i], sortArr[left], sortArr[right]])
+                while (left < right && sortArr[left] === sortArr[left + 1]) left++
+                while (left < right && sortArr[right] === sortArr[right - 1]) right--
+                left++
+                right--
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--
+            }
+        }
+    }
+    console.log(res)
+}
+getThreeAdd([-1, 0, 1, 2, -1, -4])
+
+// 接雨水
+function getRainArea(arr) {
+    let left = 0, right = arr.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let max = 0
+    while (left < right) {
+        leftMax = Math.max(leftMax, arr[left])
+        rightMax = Math.max(rightMax, arr[right])
+        if (leftMax < rightMax) {
+            max += leftMax - arr[left]
+            left++
+        } else {
+            max += rightMax - arr[right]
+            right--
+        }
+    }
+    console.log(max)
+}
+getRainArea([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+
+// 滑动窗口
+// 连续的
+function windowSlice(str) {
+    const map = new Map()
+    let maxLength = 0;
+    for (let i = 0, left = 0; i < str.length; i++) {
+        if (map.has(str[i])) {
+            left = Math.max(left, map.get(str[i]) + 1)
+        }
+        map.set(str[i], i)
+        maxLength = Math.max(maxLength, i - left + 1)
+    }
+    console.log(maxLength)
+}
+windowSlice('abcdabe')
+
+// 和为k的子数组
+function getChildArrAdd(arr, add) {
+    const map = new Map()
+    map.set(0, 1)
+    let count = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i] // 1 3 4 6
+        count += (map.get(sum - add) ?? 0) // 0 1 2 3
+        map.set(sum, (map.get(sum) ?? 0) + 1) // 1:1 3:1 4:1 6:1
+    }
+    console.log(count)
+}
+getChildArrAdd([1, 2, 1, 2], 3)
+
+// 窗口内的最大值
+// 保证窗口里的数值为size
+// 大的数值放在最前边
+function windowMax(arr, size) {
+    const maxArr = new Array(arr.length - size + 1)
+    // 存的index
+    const que = []
+    for (let i = 0; i < arr.length; i++) {
+        // 出队
+        if (que.length > 0 && que[0] < i - size + 1) {
+            que.shift()
+        }
+        while (que.length > 0 && arr[que[que.length - 1]] < arr[i]) {
+            que.pop()
+        }
+        que.push(i)
+        if (i >= size - 1) {
+            maxArr[i - size + 1] = arr[que[0]]
+        }
+    }
+    console.log(maxArr)
+}
+windowMax([3, 1, 4, 2], 2)
